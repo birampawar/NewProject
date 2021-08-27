3a23c203ba822a3dc6bfc373effc2e78dbcbbb0c
shan0088 <49829389+shan0088@users.noreply.github.com>
2021-08-27 13:23:47 +0530

    Update App.js

--- New Project/react-crud/src/App.js
+++ New Project/react-crud/src/App.js
@@ -36,9 +36,8 @@ class App extends Component {
     return (
         <div className="container mt-3">
           <Header />
-          <Search/>
           {/* <Home/> */}
-         {/* {this.state.Type === "employee" ? <Update propobj={this.state.EMPID} /> : this.state.Type === "HR" ? <Search propObj={this.state.Type}/> : this.state.Type === "Admin" ? <Admin propObj={this.state.Type}/> : <Login handleChange = {this.handleChange}/> } */}
+         {this.state.Type === "employee" ? <Update propobj={this.state.EMPID} /> : this.state.Type === "HR" ? <Search propObj={this.state.Type}/> : this.state.Type === "Admin" ? <Admin propObj={this.state.Type}/> : <Login handleChange = {this.handleChange}/> }
               
                               {/* {this.state.Type ==="employee" ? <Update propobj={this.state.EMPID} />:<Login />}
           {/* { this.state.isLogin ? <Login /> : <Register handleChange = {this.handleChange }/>} */}
