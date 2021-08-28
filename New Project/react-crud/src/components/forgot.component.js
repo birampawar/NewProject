a04ae1d675229f80233a2a4ad24839e4543592eb
shan0088 <49829389+shan0088@users.noreply.github.com>
2021-08-28 12:51:21 +0530

    Add files via upload

---
+++ New Project/react-crud/src/components/forgot.component.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import EmployeeDataService from "../services/employee.service";
+import '../css/main.css';
+
+export default class Forgot extends Component {
+  
+  render() {
+    return (      
+        <div>
+            <link
+                rel="stylesheet"
+                href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+                integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+                crossorigin="anonymous"
+            />
+            {/* Main Content */}
+           
+            <div className="search" >
+                <input required className="small" type="email" name="email" id="email" placeholder="Enter your email address"></input>
+                <input required className="small btnsubmit" type="submit" value="Send OTP"></input>
+                <input required className="small" type="number" name="OTP" maxLength="4" id="OTP" placeholder="Enter the OTP you received in email"></input>
+                <input required className="small btnsubmit" type="submit" value="Verify"></input>
+                <input required className="small" type="password" minLength="6" name="password" id="password" placeholder="password"></input>
+                <input required className="small" type="password" minLength="6" name="conpassword" id="conpassword" placeholder="Confirm Password"></input>
+                <input required className="small btnsubmit" onclick="matchPassword()" type="submit" value="RESET"></input>
+            </div>
+            
+        </div>
+        
+    )
+   
+  }
+}
---
+++ New Project/react-crud/src/components/search.component.js
@@ -0,0 +1,172 @@
+import React, { Component } from "react";
+import EmployeeDataService from "../services/employee.service";
+import profilepic from '../images/photo1.png'
+import '../css/list.css'
+import { Link } from "react-router-dom";
+import Update from "./update.component";
+import Header from "./header";
+import reactDom from "react-dom";
+export default class Search extends Component {
+  constructor(props) {
+        super(props);        
+        this.OnChangeSearchskill = this.OnChangeSearchskill.bind(this);
+        this.searchskill = this.searchskill.bind(this);
+        this.showDetails =  this.showDetails.bind(this);
+        this.setActiveEmployee = this.setActiveEmployee.bind(this);
+        this.EditEmployee = this.EditEmployee.bind(this);
+
+        this.state={
+            employees:[],
+            currentEmployee: null,
+            currentIndex: -1,
+            search :"",
+            empid : null,
+            isEdit:false
+        }
+    };
+    showDetails()
+    {
+      const currentEmployee=this.state.currentEmployee;
+      return(<React.Fragment>
+        {currentEmployee ? (
+              <div className="col details">
+                  <div className="row">
+                      <div className="col-3">
+                          <img className="avatar" src={profilepic} alt=""></img>
+                      </div>
+                      <div className="col">
+                          <h3> {currentEmployee.EmailId} </h3>
+                          <h6>{currentEmployee.Id}</h6>
+                          <h6>{currentEmployee.UserName}</h6>
+                          <h6>{currentEmployee.Domain}</h6>
+                      </div>
+                  </div>
+                  <br/>
+                  <div className="row" >
+                      <div className="col-4">
+                          <ul type="none">
+                              <li> <b> <h5> Skills </h5> </b> </li>
+                                  <ul>
+                                      <li>{currentEmployee.Skillsets}</li>
+                                  </ul>
+                              <li> <b> <h5> Tools </h5> </b> </li>
+                                  <ul>
+                                      <li>{currentEmployee.Tools}</li>
+                                  </ul>
+                              <li> <b> <h5> Domain </h5> </b> </li>
+                                  <ul>
+                                      <li>{currentEmployee.Domain}</li>
+                                  </ul>
+                              <li> <b> <h5> Experience </h5> </b> </li>
+                                  <ul>
+                                      <li>{currentEmployee.Experience}</li>
+                                  </ul>
+                          </ul>
+                      </div>
+                      <div className="col">
+                          <ul type="none">
+                              <li> <b> <h5> Project Title </h5> </b> </li>
+                                  <ul>
+                                      <li> Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta porro similique eligendi minus recusandae, deleniti quaerat aut officiis commodi veritatis magnam odio, unde natus omnis assumenda explicabo amet molestiae error?  </li>
+                                  </ul>
+                          </ul>
+                      </div>
+                  </div>
+                  {/* <center><button className="btn btn-primary">Edit</button></center> */}
+              </div>
+              ): (
+                  <div className="col details" >
+                    <br/>
+                    <p></p>
+                  </div>
+                )}
+      </React.Fragment>)
+    }
+
+    OnChangeSearchskill(e) {
+    this.setState({
+      search: e.target.value
+    });
+  }
+
+  setActiveEmployee(employee, index) {
+    console.log("setActive");
+    this.setState({
+      currentEmployee: employee,
+      currentIndex: index,
+      isEdit:false
+    });
+  }
+
+  EditEmployee(employee,index){
+    console.log("ineditemployee",employee.id)
+    this.setState({
+      currentEmployee:employee,
+      currentIndex:index,
+      isEdit:true
+    })
+  }
+
+  searchskill() {
+    var data = {
+        search: this.state.search
+      };
+    EmployeeDataService.findSkills(data)
+      .then(response => {
+        this.setState({
+          employees: response.data
+        });
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+  
+  render() {
+    const { search, employees, currentEmployee, currentIndex } = this.state;
+    console.log("in Search Page");
+
+    return (
+      <div action="">
+        {this.props.propObj === "Admin" ?<></>:<Header />}
+          <div className="row" >
+              <div className="col-5" >
+                  <ul className="list-group" type="none">
+                      <li><input type="text"className="prop" 
+                      placeholder="Search by Skills, Tools, Domain" 
+                      value={search} onChange={this.OnChangeSearchskill}></input> <button onClick={this.searchskill} className="btn btn-primary">Search</button></li>
+                  </ul>
+                  
+                  <ul className="list-group">
+                      {employees && employees.map((employee, index) => (
+                          <li className={"list-group-item " +(index === currentIndex ? "active" : "")}>
+                         <div className="row">
+                              <div className="col-8" onClick={() => this.setActiveEmployee(employee, index)}key={index}>
+                              <b> {employee.UserName} </b>  <p>{employee.Domain}</p>
+                              </div>
+                              {this.props.propObj==="Admin"?
+                              <div className="col right">
+                                
+                                <button type="button" class="btn listbtn btn-success" onClick={()=>this.EditEmployee(employee,index)}><i class="fa fa-pencil fa-2x" aria-hidden="true"></i></button>
+                                <button type="button" class="btn listbtn btn-danger"><i className="fa fa-trash fa-2x"></i></button>
+
+                              </div>
+                              : <></>}
+                            </div>
+
+                          </li>
+
+                      ))}
+                  </ul>
+              </div>
+              {/* {this.showDetails()} */}
+
+                        {this.state.isEdit ? <div className="col"><Update displayHeader={false} propobj={this.state.currentEmployee.id} /></div> : this.showDetails()}
+          </div>
+
+          
+      </div>
+    );
+  }
+}
